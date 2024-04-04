d7afd4176f00638c952d92852da2d4597355c135
Avre Barra <avrebarra@gmail.com>
2024-04-05 04:09:18 +0700

    setup base functional layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,27 @@
 function App() {
-  return <></>;
+  return (
+    <>
+      <div className="control">
+        <div className="playback-control">
+          <button type="button">play</button>
+          <button type="button">pause</button>
+          <button type="button">stop</button>
+        </div>
+        <div className="recording-control">
+          <button type="button">record</button>
+          <button type="button">pause</button>
+          <button type="button">stop</button>
+        </div>
+      </div>
+      <div className="library">
+        <div className="media-item">
+          <button type="button">play</button>
+          <button type="button">delete</button>
+          <button type="button">download</button>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
